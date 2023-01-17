b817d6ee8b4d3162236a0eefcf70ddb292ce7fc5
Tom Papke <tom.papke@tum.de>
2023-01-17 19:26:13 +0100

    Use sensible default values

--- src/ui/components/PlanPicker.tsx
+++ src/ui/components/PlanPicker.tsx
@@ -1,20 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {
-    Box,
-    Chip,
-    Divider,
-    FormControl,
-    FormControlLabel,
-    IconButton,
-    Paper,
-    Popover,
-    Radio,
-    RadioGroup,
-    Stack
+    Box, Chip, Divider, FormControl, FormControlLabel, IconButton, Paper, Popover, Radio, RadioGroup, Stack
 } from "@mui/material";
-import {
-    useAllLabels, useQueryPlanState, useUniqueDbms
-} from "../data/QueryPlanResultStore";
+import {useAllLabels, useQueryPlanState, useUniqueDbms} from "../data/QueryPlanResultStore";
 import {Nullable} from "../../semantic-diff/Types";
 import {DBMS} from "../model/meta/DBMS";
 import {max, scaleLinear as d3ScaleLinear} from "d3";
@@ -31,34 +19,24 @@ const worseColorScale = d3ScaleLinear<string>()
     .domain([0, 1])
     .range(['#808080', '#ff0000']); //red
 
-export default function PlanPicker (props: IQueryPlanResultDiffProps) {
+export default function PlanPicker(props: IQueryPlanResultDiffProps) {
 
-    const [baselineDbms, setBaseLineDbms] = useState(
-        DBMS.Umbra as Nullable<DBMS>);
-    const [selectedQuery, setSelectedQuery] = useState(
-        null as Nullable<string>);
-    const [compDbms, setCompDbms] = useState(null as Nullable<DBMS>);
+    const [baselineDbms, setBaseLineDbms] = useState(DBMS.Umbra as Nullable<DBMS>);
+    const [selectedQuery, setSelectedQuery] = useState("8.sql" as Nullable<string>);
+    const [compDbms, setCompDbms] = useState(DBMS.Hyper as Nullable<DBMS>);
     const [state, actions] = useQueryPlanState();
 
     useEffect(() => {
         if (baselineDbms && selectedQuery && compDbms) {
-            const firstPlanResult = state.queryPlanResults.find(
-                qpr => qpr.dbms ===
-                       baselineDbms &&
-                       qpr.queryName ===
-                       selectedQuery)!!;
-            const secondPlanResult = state.queryPlanResults.find(
-                qpr => qpr.dbms ===
-                       compDbms &&
-                       qpr.queryName ===
-                       selectedQuery)!!;
+            const firstPlanResult = state.queryPlanResults.find(qpr => qpr.dbms === baselineDbms && qpr.queryName === selectedQuery)!!;
+            const secondPlanResult = state.queryPlanResults.find(qpr => qpr.dbms === compDbms && qpr.queryName === selectedQuery)!!;
             actions.setResultSelection([firstPlanResult, secondPlanResult]);
         } else {
             actions.setResultSelection(null);
         }
     }, [baselineDbms, selectedQuery, compDbms])
 
-    function resetBaseline (newBaseline: DBMS) {
+    function resetBaseline(newBaseline: DBMS) {
         setSelectedQuery(null);
         setCompDbms(null);
         setBaseLineDbms(newBaseline);
@@ -70,11 +48,9 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
     // dbms
     const [availableDbms] = useUniqueDbms();
 
-    const qprForSelectedQuery = state.queryPlanResults.filter(
-        qpr => qpr.queryName === selectedQuery);
+    const qprForSelectedQuery = state.queryPlanResults.filter(qpr => qpr.queryName === selectedQuery);
 
-    const baseLineQprForSelectedQuery = state.queryPlanResults.find(
-        qpr => qpr.queryName === selectedQuery && qpr.dbms === baselineDbms);
+    const baseLineQprForSelectedQuery = state.queryPlanResults.find(qpr => qpr.queryName === selectedQuery && qpr.dbms === baselineDbms);
 
     const QueriesSet = new Set(state.queryPlanResults.map(qpr => qpr.queryName))
     const uniqueQueries = Array.from(QueriesSet.entries())
@@ -82,8 +58,7 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
 
     const worstResultsPerQuery = uniqueQueries.map(query => ({
         query,
-        baseLineResult: state.queryPlanResults.find(
-            qpr => qpr.dbms === baselineDbms && qpr.queryName === query)!!,
+        baseLineResult: state.queryPlanResults.find(qpr => qpr.dbms === baselineDbms && qpr.queryName === query)!!,
         otherResults: state.queryPlanResults
             .filter(qpr => {
                 return qpr.dbms !== baselineDbms && qpr.queryName === query
@@ -93,8 +68,7 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
             ...obj,
             worstDiff: max(obj.otherResults
                 .map(qpr => {
-                    if (!qpr.benchmarkResult.total ||
-                        !obj.baseLineResult.benchmarkResult.total) {
+                    if (!qpr.benchmarkResult.total || !obj.baseLineResult.benchmarkResult.total) {
                         return null;
                     } else {
                         const baseLineMetric = obj.baseLineResult.benchmarkResult.total;
@@ -111,14 +85,11 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
         .sort((a, b) => b.worstDiff - a.worstDiff);
 
     const worstOverallDiff = worstResultsPerQuery[0].worstDiff;
-    const bestOverallDiff = worstResultsPerQuery[worstResultsPerQuery.length -
-                                                 1].worstDiff;
+    const bestOverallDiff = worstResultsPerQuery[worstResultsPerQuery.length - 1].worstDiff;
 
-    function QueryComponent (props: {}) {
-        const [listAnchorEl, setListAnchorEl] = useState(
-            null as Nullable<HTMLElement>);
-        const [textAnchorEl, setTextAnchorEl] = useState(
-            null as Nullable<HTMLElement>);
+    function QueryComponent(props: {}) {
+        const [listAnchorEl, setListAnchorEl] = useState(null as Nullable<HTMLElement>);
+        const [textAnchorEl, setTextAnchorEl] = useState(null as Nullable<HTMLElement>);
 
         const QueryRadios = worstResultsPerQuery
             .map(obj => {
@@ -129,14 +100,8 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
 
                 // if worstOtherDbmsResult is negative, then our time is
                 // increased in comparison
-                const label = query +
-                              " (" +
-                              (worstDiff < 0 ? "" : "+") +
-                              (worstDiff * 100).toFixed(0) +
-                              "%)";
-                const color = (worstDiff < 0) ? betterColorScale(
-                    worstDiff / bestOverallDiff) : worseColorScale(
-                    worstDiff / worstOverallDiff);
+                const label = query + " (" + (worstDiff < 0 ? "" : "+") + (worstDiff * 100).toFixed(0) + "%)";
+                const color = (worstDiff < 0) ? betterColorScale(worstDiff / bestOverallDiff) : worseColorScale(worstDiff / worstOverallDiff);
                 return (<Stack key={query}
                                direction="row"
                                justifyContent="start"
@@ -175,8 +140,7 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
                 </Popover>
                 {selectedQuery != null && <>
                     <IconButton
-                        onClick={(event) => setTextAnchorEl(
-                            event.currentTarget)}>
+                        onClick={(event) => setTextAnchorEl(event.currentTarget)}>
                         <Subject/>
                     </IconButton>
                     <Popover
@@ -197,7 +161,7 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
     }
 
 
-    function BaselineComponent (props: {}) {
+    function BaselineComponent(props: {}) {
         const [anchorEl, setAnchorEl] = useState(null as Nullable<HTMLElement>);
 
         const BaselineRadios = availableDbms.map(dbms => {
@@ -212,13 +176,11 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
                 <h3>Baseline</h3>
             </Box>
             <Stack direction="row">
-                <IconButton onClick={(event) => setAnchorEl(
-                    event.currentTarget)}><Edit/></IconButton>
+                <IconButton onClick={(event) => setAnchorEl(event.currentTarget)}><Edit/></IconButton>
                 <Popover anchorEl={anchorEl} open={anchorEl != null}
                          onClose={() => setAnchorEl(null)}>
                     <FormControl>
-                        <RadioGroup onChange={(event) => resetBaseline(
-                            event.target.value as DBMS)}>
+                        <RadioGroup onChange={(event) => resetBaseline(event.target.value as DBMS)}>
                             {BaselineRadios}
                         </RadioGroup>
                     </FormControl>
@@ -227,14 +189,13 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
                        alignItems="center"
                        justifyContent="center">
                     <Chip color="primary"
-                          label={baselineDbms ??
-                                 "Please select a baseline"}></Chip>
+                          label={baselineDbms ?? "Please select a baseline"}></Chip>
                 </Stack>
             </Stack>
         </Stack>)
     }
 
-    function CompComponent (props: {}) {
+    function CompComponent(props: {}) {
         const CompCandidateItems = qprForSelectedQuery
             .filter(qpr => qpr != baseLineQprForSelectedQuery)
             .map(qpr => {
@@ -242,17 +203,9 @@ export default function PlanPicker (props: IQueryPlanResultDiffProps) {
 
                 let addLabel = "";
                 if (baseLineQprForSelectedQuery) {
-                    const diff = baseLineQprForSelectedQuery.benchmarkResult.total!! /
-                                 qpr.benchmarkResult.total!! -
-                                 1;
-                    addLabel =
-                        " (" +
-                        (diff < 0 ? "" : "+") +
-                        (diff * 100).toFixed(0) +
-                        "%)";
-                    const color = (diff < 0) ? betterColorScale(
-                        diff / bestOverallDiff) : worseColorScale(
-                        diff / worstOverallDiff);
+                    const diff = baseLineQprForSelectedQuery.benchmarkResult.total!! / qpr.benchmarkResult.total!! - 1;
+                    addLabel = " (" + (diff < 0 ? "" : "+") + (diff * 100).toFixed(0) + "%)";
+                    const color = (diff < 0) ? betterColorScale(diff / bestOverallDiff) : worseColorScale(diff / worstOverallDiff);
                 }
 
                 return (<Chip
