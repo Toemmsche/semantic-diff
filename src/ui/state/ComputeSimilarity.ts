1f0759337eefe7fdc40a65bd49ab98db3c9af59e
Tom Papke <tom.papke@outlook.de>
2023-02-19 19:21:57 +0100

    simplify and speed up similarity computation

--- src/ui/state/ComputeSimilarity.ts
+++ src/ui/state/ComputeSimilarity.ts
@@ -37,37 +37,41 @@ export default function computeSimilarity(
 
         // advanced similarity measure
         // compute commonality for all matched nodes and weight it by cardinality
-        const commonalities: number[] = [];
+        const cvs: number[] = [];
         const weights: number[] = [];
+
+        // treat unmatched inners
+        [
+          ...plans[0].toPreOrderUnique(),
+          ...plans[1].toPreOrderUnique()
+        ].filter(n => !n.isMatched() && !n.isLeaf())
+         .forEach(n => {
+           let cardinality = n.data.exactCardinality;
+           if (cardinality === 0) {
+             cardinality = 1;
+           }
+           cvs.push(0);
+           // use half the weight since unmatched nodes appear in both trees
+           weights.push(Math.log2(cardinality) / 2);
+         });
+
+        // treat matched inners
         plans[0]
           .toPreOrderUnique()
           .filter((n) => n.isMatched() && !n.isLeaf())
           .forEach((n) => {
-            // simple similarity measure = (# matched) / max(sizeSubtree, sizeMatchSubtree)
-            const firstSet = new Set(n.toPreOrderUnique());
-            const secondSet = new Set(n.getSingleMatch().toPreOrderUnique());
-
-            let common = 0;
-            for (const cand of firstSet) {
-              if (cand.isMatched() && secondSet.has(cand.getSingleMatch())) {
-                common++;
-              }
-            }
-
             let avgCardinality =
               (n.data.exactCardinality + n.getSingleMatch().data.exactCardinality) / 2;
-            const maxSize = Math.max(firstSet.size, secondSet.size);
-            const commonality = common / maxSize;
-
-            commonalities.push(commonality);
-            // weighted by size of tree (=number of operators) and importance of operators (=cardinality)
-            weights.push(
-              (avgCardinality === 0 ? 1 : Math.log2(avgCardinality)) * Math.log2(maxSize)
+            if (avgCardinality === 0) {
+              avgCardinality = 1;
+            }
+            cvs.push(1);
+            weights.push(Math.log2(avgCardinality)
             );
           });
 
         let similarity = new Comparator(defaultDiffOptions).weightedAverage(
-          commonalities,
+          cvs,
           weights,
           1
         );
